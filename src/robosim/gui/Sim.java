288680f990953b1a2f77648dd146a2ab41c8ca13
gjf2a <ferrer@hendrix.edu>
2022-01-10 17:14:29 -0600

    Continued Refactoring.

--- src/robosim/gui/Sim.java
+++ src/robosim/gui/Sim.java
@@ -1,11 +1,19 @@
 package robosim.gui;
 
+import handwriting.gui.DrawingEditor;
 import robosim.ai.Controller;
 import robosim.core.SimObjMaker;
 import robosim.core.Simulator;
 import search.core.AIReflector;
 
 import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.util.stream.Stream;
 
 public class Sim extends JFrame {
     public static final double RADIUS = 10.0;
@@ -17,16 +25,67 @@ public class Sim extends JFrame {
     JButton start, stop, singleStep, resume, reset;
     JTextField total, forward, collisions;
     JTextArea messages;
+    SimPanel sim;
 
     Simulator map;
 
     AIReflector<Controller> ais;
 
-    Timer timer;
-
     JMenuItem open, save, close;
 
     public Sim() {
+        setSize(700, 700);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ais = new AIReflector<>(SimObjMaker.class, "robosim.ai");
 
+        getContentPane().setLayout(new BorderLayout());
+
+        sim = new SimPanel();
+        getContentPane().add(sim, BorderLayout.CENTER);
+
+        JMenuBar bar = new JMenuBar();
+        setJMenuBar(bar);
+        addFileMenu(bar);
+    }
+
+    private void addFileMenu(JMenuBar bar) {
+        JMenu fileMenu = new JMenu("File");
+        bar.add(fileMenu);
+
+        open = new JMenuItem("Open");
+        open.addActionListener(e -> {
+            JFileChooser chooser = new JFileChooser();
+            if (chooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
+                try {
+                    File openee = chooser.getSelectedFile();
+                    Stream<String> stream = Files.lines(openee.toPath());
+                    StringBuilder sb = new StringBuilder();
+                    stream.forEach(s -> sb.append(s).append("\n"));
+                    map.resetObjectsFrom(sb.toString());
+                    map.drawOn(sim);
+                } catch (IOException ex) {
+                    oops(ex);
+                }
+            }});
+        fileMenu.add(open);
+
+        save = new JMenuItem("Save");
+        save.addActionListener(e -> {
+            JFileChooser chooser = new JFileChooser();
+            if (chooser.showSaveDialog(null) == JFileChooser.APPROVE_OPTION) {
+                try {
+                    File savee = chooser.getSelectedFile();
+                    PrintWriter out = new PrintWriter(savee);
+                    out.println(map.getMapString());
+                    out.close();
+                } catch (FileNotFoundException ex) {
+                    oops(ex);
+                }
+            }
+        });
+        fileMenu.add(save);
+    }
+
+    private void oops(IOException e) {
     }
 }
---
+++ src/robosim/gui/SimPanel.java
@@ -0,0 +1,13 @@
+package robosim.gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class SimPanel extends JPanel {
+    Timer timer;
+
+    @Override
+    public void paintComponent(Graphics g) {
+
+    }
+}
