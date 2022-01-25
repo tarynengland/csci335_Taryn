1f2553bb0dae642e9fe3eb5bca5cd767a1f4a07d
gjf2a <ferrer@hendrix.edu>
2022-01-25 10:25:17 -0600

    Fixed bug. Checkers should run now.

--- src/checkers/evaluators/NoPreference.java
+++ src/checkers/evaluators/NoPreference.java
@@ -3,9 +3,10 @@ package checkers.evaluators;
 import checkers.core.Checkerboard;
 
 import java.util.function.ToDoubleFunction;
+import java.util.function.ToIntFunction;
 
-public class NoPreference implements ToDoubleFunction<Checkerboard> {
-    public double applyAsDouble(Checkerboard c) {
+public class NoPreference implements ToIntFunction<Checkerboard> {
+    public int applyAsInt(Checkerboard c) {
         return 0;
     }
 }
